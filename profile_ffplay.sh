936bba03cca0abaf28c03445a836e9f39803bf6f
linuxcao <527121128@qq.com>
2016-01-27 15:10:19 +0800

    add mkdir -p /usr/share/ffplay in profile_ffplay.sh

--- profile_ffplay.sh
+++ profile_ffplay.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
+mkdir -p /usr/share/ffplay
+
 cp -f ./res/picture/*.png /usr/share/ffplay/
 
 cp -fr ./res/gtk-2.0/* /etc/gtk-2.0/
 
 tar xvf ./ffmpeg/lib/ffmpeg.lib.tar.gz -C /usr/lib
 
-cp -f ./bin/ffplay /usr/bin/ 
+cp -f ./bin/gtk-ffplay /usr/bin/ 
